4555667cc4c41ed87d385566ea370dff67276b22
thelouisgram <hadesanoye01@gmail.com>
2026-02-02 22:40:58 +0100

    fix: correct `StartPagina.svg` image source path casing to lowercase.

--- app/features/sidebar/SideBar.tsx
+++ app/features/sidebar/SideBar.tsx
@@ -40,7 +40,7 @@ const Sidebar: React.FC<SidebarProps> = ({
         width={20}
         height={20}
         alt="Start Pagina"
-        src="./assets/StartPagina.svg"
+        src="./assets/startpagina.svg"
       />
     );
   };
