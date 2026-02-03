14d1c7fa81e4ad358a1aaeaceb02a5779d1df858
thelouisgram <hadesanoye01@gmail.com>
2026-02-03 23:21:49 +0100

    Remove hover effects from calendar events and grid cells.

--- app/features/mainContent/Pages/planner/components/CalendarGrid/MonthView.tsx
+++ app/features/mainContent/Pages/planner/components/CalendarGrid/MonthView.tsx
@@ -70,7 +70,6 @@ const MonthView = ({ selectedDate }: MonthViewProps) => {
               borderRight="1px solid #D9E5F2"
               borderBottom="1px solid #D9E5F2"
               bg={!isCurrentMonth ? "#F9FAFB" : "white"}
-              _hover={{ bg: "#f0f2ff", shadow: "inset 0 0 0 1px #5653FC33" }}
               cursor="pointer"
               transition="all 0.2s ease-in-out"
             >
